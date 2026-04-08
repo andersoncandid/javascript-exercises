24c902ff5cf93847ccad4199c3f9e15acc0af2cd
andersoncandid <190989879+andersoncandid@users.noreply.github.com>
2026-04-08 06:55:23 -0300

    finish 13

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((acc, current) => acc += current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((acc, current) => acc * current, 1);
 };
 
-const power = function() {
-	
+const power = function (base, exp) {
+  const result = Math.pow(base, exp);
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0) {
+    return 1;
+  }
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +33,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let strArray = str.toLowerCase().replace(/[^\w]/g, "").split("");
+  const length = strArray.length;
+  const middle = Math.floor(length / 2);
 
+  for (let i = 0; i < middle; i++) {
+    if (strArray[i] != strArray[length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
